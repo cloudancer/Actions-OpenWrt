54fdddc7b08e689e8e21e68f69680ca1201ae10d
vagrant <hinianjian@126.com>
2021-04-20 07:45:32 +0800

    full package

---
+++ diy-cp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# add packages                                                                                                     
+rm -rf package/lean/{k3screenctrl,luci-app-syncdial,luci-theme-argon}
+
+git clone https://github.com/garypang13/luci-app-eqos.git package/lean/luci-app-eqos
+git clone https://github.com/lwz322/k3screenctrl.git package/lean/k3screenctrl
+git clone https://github.com/lwz322/k3screenctrl_build.git package/lean/k3screenctrl_build
+git clone https://github.com/lwz322/luci-app-k3screenctrl.git package/lean/luci-app-k3screenctrl
+git clone https://github.com/sirpdboy/luci-app-autotimeset.git package/lean/luci-app-autotimeset
+git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/luci-app-poweroff
+git clone https://github.com/rufengsuixing/luci-app-syncdial.git package/lean/luci-app-syncdial
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+
--- diy-part2.sh
+++ diy-part2.sh
@@ -13,29 +13,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generate
 
-#Update Argon and modify default theme
-rm -rf package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+# modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i 's/bootstrap/argon/g' feeds/luci/modules/luci-base/root/etc/config/luci
 
-# add extra package I want to use
-git clone https://github.com/xiangfeidexiaohuo/openwrt-packages.git
-
-rm -rf feeds/luci/applications/luci-app-udpxy/luasrc/model/cbi/udpxy.lua
-rm -rf feeds/luci/applications/luci-app-mwan3/po/zh-cn/mwan3.po
-mv openwrt-packages/files/udpxy.lua feeds/luci/applications/luci-app-udpxy/luasrc/model/cbi/
-mv openwrt-packages/files/mwan3.po feeds/luci/applications/luci-app-mwan3/po/zh-cn/
-
-rm -rf package/lean/{k3screenctrl,luci-app-syncdial,luci-app-diskman,luci-app-sfe}
-
-dirs="k3screenctrl,k3screenctrl_build,luci-app-k3screenctrl,luci-app-autotimeset,luci-app-poweroff,luci-app-syncdial,luci-app-smartdns,smartdns,luci-app-eqos,luci-app-sfe"                                                                                                
-OLD_IFS="$IFS"
-IFS=","
-array=($dirs)
-for i in ${array[@]}
-do
-    cp -rf openwrt-packages/$i package/lean/
-done
-
-rm -rf openwrt-packages
