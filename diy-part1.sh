56d0573576f287711841c412002e58eee1a39897
cloudancer <hinianjian@126.com>
2022-01-16 16:34:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,9 +10,26 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# delete old packages
+rm -rf ./package/lean/k3screenctrl
+rm -rf ./package/lean/luci-app-syncdial
+rm -rf ./package/lean/luci-theme-argon
+rm -rf ./package/lean/luci-app-jd-dailybonus
+rm -rf ./package/lean/luci-lib-docker
+rm -rf ./package/lean/luci-app-pushbot
+rm -rf ./package/lean/luci-app-netdata
+rm -rf ./package/lean/aliyundrive-webdav
+rm -rf ./package/lean/luci-app-aliyundrive-webdav
+rm -rf ./package/lean/go-aliyundrive-webdav
+rm -rf ./package/lean/luci-app-go-aliyundrive-webdav
+rm -rf ./package/lean/luci-app-easymesh
+rm -rf ./package/lean/autocore
+rm -rf ./package/lean/luci-app-cpufreq
+rm -rf ./package/lean/luci-app-kodexplorer
 
-# Add a feed source
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# add a packages source
+echo -e "\nsrc-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages" >> feeds.conf.default
+
+# add new theme argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
