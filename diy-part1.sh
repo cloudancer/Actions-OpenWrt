69a1dae28dcea27e9f4eedc77f92739d69400607
cloudancer <hinianjian@126.com>
2022-03-08 09:40:26 +0800

    clean

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,24 +11,7 @@
 #
 
 # delete old packages
-rm -rf ./package/lean/k3screenctrl
-rm -rf ./package/lean/luci-app-syncdial
 rm -rf ./package/lean/luci-theme-argon
-rm -rf ./package/lean/luci-app-jd-dailybonus
-rm -rf ./package/lean/luci-lib-docker
-rm -rf ./package/lean/luci-app-pushbot
-rm -rf ./package/lean/luci-app-netdata
-rm -rf ./package/lean/aliyundrive-webdav
-rm -rf ./package/lean/luci-app-aliyundrive-webdav
-rm -rf ./package/lean/go-aliyundrive-webdav
-rm -rf ./package/lean/luci-app-go-aliyundrive-webdav
-rm -rf ./package/lean/luci-app-easymesh
-rm -rf ./package/lean/autocore
-rm -rf ./package/lean/luci-app-cpufreq
-rm -rf ./package/lean/luci-app-kodexplorer
-
-# add a packages source
-echo -e "\nsrc-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages" >> feeds.conf.default
 
 # add new theme argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
