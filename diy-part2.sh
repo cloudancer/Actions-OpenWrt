4fab06e06a6098fcb1468332ead11e2ab7540adf
cloudancer <hinianjian@126.com>
2022-03-10 09:30:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,3 +20,11 @@ sed -i "s/hostname='OpenWrt'/hostname='cloudancer'/g" package/base-files/files/b
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i 's/bootstrap/argon/g' feeds/luci/modules/luci-base/root/etc/config/luci
 
+# Modify default ssid
+sed -i 's/OpenWrt/cloudancer/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# Modify default wifi encryption
+sed -i 's/none/psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# Add default wifi password
+sed -i '/psk2/a\ \t\t\tset wireless.default_radio${devidx}.key=passw0rD' package/kernel/mac80211/files/lib/wifi/mac80211.sh
