3bfe41af04b769eb75521f8543f133526243016d
cloudancer <hinianjian@126.com>
2022-03-14 08:42:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,7 +18,7 @@ git clone https://github.com/yangxu52/luci-app-k3screenctrl.git package/lean/luc
 git clone https://github.com/yangxu52/k3screenctrl_build.git package/lean/k3screenctrl/
 
 # new theme argon
-#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
 # '替换K3的无线驱动为69027'
 #wget -nv https://github.com/Hill-98/phicommk3-firmware/raw/master/brcmfmac4366c-pcie.bin.69027 -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
